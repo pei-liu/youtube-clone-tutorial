8f0927261a8c918ca7bab7e6d0619d21d922aafb
Pei Xiong Liu <pliu@fiksu.com>
2016-01-25 20:35:46 -0500

    searchbar working

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -14,10 +14,15 @@ class SearchBar extends React.Component {
       <div className="search-bar">
         <input
           value={this.state.term}
-          onChange={(event) => this.setState({term: event.target.value})}/>
+          onChange={event => this.onInputChange(event.target.value)} />
       </div>
     );
   }
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
 }
+
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -14,18 +14,22 @@ class App extends Component {
       videos: [],
       selectedVideo: null,
     };
+    this.videoSearch('ukelele')
+  }
 
-    YTSearch({key: API_KEY, term: 'ukelele'}, (videos) => {
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0],
       });
     });
   }
+
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           videos={this.state.videos}
